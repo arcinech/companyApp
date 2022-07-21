7a55493f7ca97c5497c95f3e850aee7c25f84665
Marcin Lech <arcinech@gmail.com>
2022-07-21 16:50:29 +0200

    Fix: employe delete route

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -69,7 +69,7 @@ router.delete('/employees/:id', async (req, res) => {
   try { 
     const emp = await Employee.findById(req.params.id);
     if(emp) {
-      await Department.deleteOne({ _id: req.params.id });
+      await Employee.deleteOne({ _id: req.params.id });
       res.json({ message: 'OK' });
     }
     else res.status(404).json({ message: 'Not found...' });
