087946b55e556d73ffe8578e6fe408bb944062c4
Marcin Lech <arcinech@gmail.com>
2022-07-27 13:09:18 +0200

    Update: server for testing

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const productsRoutes = require('./routes/products.routes');
 
 const app = express();
 
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+if(NODE_ENV === 'production') dbUri = 'url to remote db';
+else if(NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/companyDBtest';
+else dbUri = 'mongodb://localhost:27017/companyDB';
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,14 +27,16 @@ app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
 });
 
-mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
-  console.log('Connected to the database');
+  // console.log('Connected to the database');
 });
 db.on('error', err => console.log('Error ' + err));
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+const server = app.listen('8000', () => {
+  // console.log('Server is running on port: 8000');
 });
+
+module.exports = server;
